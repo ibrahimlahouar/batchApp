e94328b9cd0a7510c78436a6f307bcc7d7a5e985
ilahouar <dev.ilahouar@gmail.com>
2025-05-23 13:00:15 +0200

    Initialize Spring Boot Batch Application with Maven setup
    Added initial structure for a batch processing application, configured with Maven Wrapper, PostgreSQL, and Trino. Includes basic dependencies and a functional Spring Boot application skeleton with test configuration.

---
+++ src/main/java/com/ilahouar/batchapp/BatchAppApplication.java
@@ -0,0 +1,18 @@
+package com.ilahouar.batchapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Application principale pour le traitement batch de données entre Trino et PostgreSQL
+ * 
+ * Ce batch est conçu pour lire des données depuis une source Trino et les écrire dans PostgreSQL
+ * avec un traitement parallèle pour gérer efficacement les grands volumes de données.
+ */
+@SpringBootApplication
+public class BatchAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchAppApplication.class, args);
+    }
+} 
